6c068a4fb0019f3c493da0861d24eeb5a9b01531
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-07-27 22:42:54 +0300

    Update Vagrant and script.sh

--- lesson_01/script.sh
+++ lesson_01/script.sh
@@ -2,8 +2,18 @@
 
 # Подготовка системы
 yum update -y
-yum install -y epel-release
-yum install -y htop lshw mc
+yum install -y mc nano wget ncurses-devel openssl-devel bc install libelf-dev, libelf-devel
+yum groupinstall -y "Development Tools"
 
 #
-uname -r >> kernel_versions.txt
+uname -r >> kernel_versions.log
+
+cd /usr/src/kernels
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.61.tar.xz
+tar -xvf linux-4.19.61.tar.xz -C .
+
+# cp /boot/config* .config
+# make oldconfig
+# make
+# make install
+# make modules_install
