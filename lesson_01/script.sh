1ce555cf89f8cb456ddb19d88b9e4df395c05b83
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-07-28 01:01:15 +0300

    Update Vagrantfile & script.sh

--- lesson_01/script.sh
+++ lesson_01/script.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
-# Подготовка системы
+# Обновление системы и установка необходимых для компиляции ядра пакетов
 yum update -y
 yum install -y mc nano wget ncurses-devel openssl-devel bc install libelf-dev, libelf-devel
 yum groupinstall -y "Development Tools"
 
-#
-uname -r >> kernel_versions.log
+# Формирование скрипта для компиляции нового ядра,
+# который необходимо запустить вручную после развертывания
+# тестового окружения
+OUTFILE=kernel_compile.sh
+(
+cat <<- '_EOF_'
+        #!/usr/bin/env bash
 
-cd /usr/src/kernels
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.61.tar.xz
-tar -xvf linux-4.19.61.tar.xz -C .
+        cd /usr/src/kernels
+        wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.61.tar.xz
+        tar -xvf linux-4.19.61.tar.xz -C .
+_EOF_
+) > $OUTFILE
+chmod +x $OUTFILE
+
+shutdown -r now
 
 # cp /boot/config* .config
 # make oldconfig
 # make
-# make install
 # make modules_install
+# make install
