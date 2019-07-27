83a61a3b1bb8d67cad9175b4f553a4e1ff302c07
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-07-28 01:44:22 +0300

    Update script.sh

--- lesson_01/script.sh
+++ lesson_01/script.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
-# Обновление системы
-yum update -y
-
-# Установка необходимых для компиляции ядра пакетов
-yum install -y mc nano wget ncurses-devel openssl-devel bc install libelf-dev libelf-devel
-yum groupinstall -y "Development Tools"
-
 # Формирование скрипта для компиляции нового ядра,
 # который необходимо запустить вручную после развертывания
 # тестового окружения
 OUTFILE=kernel_compile.sh
 (
 cat <<- '_EOF_'
-        #!/usr/bin/env bash
+    #!/usr/bin/env bash
 
-        cd /usr/src/kernels
-        wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.61.tar.xz
-        tar -xvf linux-4.19.61.tar.xz -C .
+    cd /usr/src/kernels
+    wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.61.tar.xz
+    tar -xvf linux-4.19.61.tar.xz -C .
 
-        # cp /boot/config* .config
-        # make oldconfig
-        # make
-        # make modules_install
-        # make install
-        _EOF_
+    # cp /boot/config* .config
+    # make oldconfig
+    # make
+    # make modules_install
+    # make install
+_EOF_
 ) > $OUTFILE
 chmod +x $OUTFILE
 
-# Перезагрузка система после ее обновления
+# Обновление системы
+yum update -y
+
+yum install -y mc nano wget
+# Установка необходимых для компиляции ядра пакетов
+# yum install -y ncurses-devel openssl-devel bc install libelf-dev libelf-devel
+# yum groupinstall -y "Development Tools"
+
 shutdown -r now
