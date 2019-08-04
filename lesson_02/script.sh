e9a681a0826c93544fee22e754145d540e041b2e
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-08-04 15:23:20 +0300

    Update Vagranfile, script.sh

--- lesson_02/script.sh
+++ lesson_02/script.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-function prnt_info {
-    echo "Usage: choose_raid.sh [-r raid_level]"
-    echo "  -h, --help          help"
-    echo "  -r, --raid          raid level"
-    return
-}
-
-# clear
-prnt_info
+yum update -y
+yum install -y mdadm smartmontools hdparm gdisk
