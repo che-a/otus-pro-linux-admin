28d0b0d3e4a9f96a0dd3a0054a48b1a1c66fae29
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-08-07 01:48:02 +0300

    Update Vagrantfile && script.sh

--- lesson_02/script.sh
+++ lesson_02/script.sh
@@ -1,36 +1,37 @@
 #!/usr/bin/env bash
 
 yum update -y
-yum install -y mdadm smartmontools hdparm gdisk nano
+yum install -y mdadm smartmontools hdparm gdisk
+yum install -y nano wget
 
 # Уничтожаем структуры данных GPT и MBR
-sgdisk --zap-all /dev/sdb
+# sgdisk --zap-all /dev/sdb
 
 # Очищаем таблицу разделов
-sgdisk -o /dev/sdb
+# sgdisk -o /dev/sdb
 
-sgdisk -n 1:0:+1M --typecode=1:EF02 /dev/sdb
-sgdisk -n 2:0:+512M --typecode=2:8300 /dev/sdb
-sgdisk --largest-new=3 /dev/sdb
+# sgdisk -n 1:0:+1M --typecode=1:EF02 /dev/sdb
+# sgdisk -n 2:0:+512M --typecode=2:8300 /dev/sdb
+# sgdisk --largest-new=3 /dev/sdb
 
 # Копируем таблицу разделов с /dev/sdb на /dev/sdc
-sgdisk -R /dev/sdc /dev/sdb
+# sgdisk -R /dev/sdc /dev/sdb
 
 # делаем уникальными скопированные GUID разделов
-sgdisk -G /dev/sdc
+# sgdisk -G /dev/sdc
 
 # Перемещаем второй заголовок в конец диска
-sgdisk --randomize-guids --move-second-header /dev/sdc
+# sgdisk --randomize-guids --move-second-header /dev/sdc
 
 # Создаем RAID
-mdadm --create --verbose /dev/md0 --force --level=0 --raid-devices=2 /dev/sdb3 /dev/sdc3
-mdadm --create --verbose /dev/md1 --force --level=1 --raid-devices=2 /dev/sdb2 /dev/sdc2
-mdadm --detail --scan --verbose
+# mdadm --create --verbose /dev/md0 --force --level=0 --raid-devices=2 /dev/sdb3 /dev/sdc3
+# mdadm --create --verbose /dev/md1 --force --level=1 --raid-devices=2 /dev/sdb2 /dev/sdc2
+# mdadm --detail --scan --verbose
+# df -h -x devtmpfs -x tmpfs
 
-
-mkdir /mnt/md{0,1,5,6,10}
-
-mkfs.ext4 /dev/md0
-mkfs.ext4 /dev/md1
-mount /dev/md0 /mnt/md0
-mount /dev/md1 /mnt/md1
+#
+# mkdir /mnt/md{0,1,5,6,10}
+# mkfs.ext4 /dev/md0
+# mkfs.ext4 /dev/md1
+# mount /dev/md0 /mnt/md0
+# mount /dev/md1 /mnt/md1
