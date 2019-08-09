116692e0464c7d5e155a87426e0acf1dc46215d9
Алексей Чернышенко <tech.dep.it@ya.ru>
2019-08-09 19:00:43 +0300

    Update script.sh

--- lesson_02/script.sh
+++ lesson_02/script.sh
@@ -27,16 +27,54 @@ sgdisk -G /dev/sdc
 # Перемещаем второй заголовок в конец диска
 sgdisk --randomize-guids --move-second-header /dev/sdc
 
-# Создаем RAID 0
+# Создание RAID 0
 mdadm --create --verbose /dev/md0 --force --level=0 --raid-devices=2 /dev/sdb2 /dev/sdc2
 mdadm --create --verbose /dev/md1 --force --level=0 --raid-devices=2 /dev/sdb4 /dev/sdc4
 
-# Создаем RAID 1
-# mdadm --create --verbose /dev/md2 --force --level=1 --raid-devices=2 /dev/sdb3 /dev/sdc3
-# mdadm --create --verbose /dev/md3 --force --level=1 --raid-devices=2 /dev/sdb5 /dev/sdc5
-# mdadm --create --verbose /dev/md4 --force --level=1 --raid-devices=2 /dev/sdb6 /dev/sdc6
+# Создание RAID 1
+mdadm --create --metadata=1.2 --verbose /dev/md2 --force --level=1 --raid-devices=2 /dev/sdb3 /dev/sdc3
+mdadm --create --metadata=1.2 --verbose /dev/md3 --force --level=1 --raid-devices=2 /dev/sdb5 /dev/sdc5
+mdadm --create --metadata=1.2 --verbose /dev/md4 --force --level=1 --raid-devices=2 /dev/sdb6 /dev/sdc6
 
-# mdadm --detail --scan --verbose
+# Создание RAID 5
+mdadm --create --verbose /dev/md5 --level=5 --raid-devices=4 /dev/sd{d,e,f,g}
+
+# Удаление RAID 5
+sleep 10
+mdadm --stop /dev/md5
+mdadm --zero-superblock /dev/sdd
+mdadm --zero-superblock /dev/sde
+mdadm --zero-superblock /dev/sdf
+mdadm --zero-superblock /dev/sdg
+
+# Создание RAID 6
+mdadm --create --verbose /dev/md6 --level=6 --raid-devices=4 /dev/sd{d,e,f,g}
+
+# Удаление RAID 6
+sleep 10
+mdadm --stop /dev/md6
+mdadm --zero-superblock /dev/sdd
+mdadm --zero-superblock /dev/sde
+mdadm --zero-superblock /dev/sdf
+mdadm --zero-superblock /dev/sdg
+
+# Создание RAID 10
+mdadm --create --verbose /dev/md10 --level=10 --raid-devices=4 /dev/sd{d,e,f,g}
+
+# Удаление RAID 10
+sleep 10
+mdadm --stop /dev/md10
+mdadm --zero-superblock /dev/sdd
+mdadm --zero-superblock /dev/sde
+mdadm --zero-superblock /dev/sdf
+mdadm --zero-superblock /dev/sdg
+
+
+# Создание файла конфигурации mdadm.conf
+echo "DEVICE partitions" > /etc/mdadm.conf
+mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm.conf
+
+# shutdown -r now
 # df -h -x devtmpfs -x tmpfs
 
 #
