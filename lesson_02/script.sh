dfd7c57dd94949ba60fbdd6605896d9159dd888d
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-08-11 17:27:31 +0300

    Correct code for transfer to RAID.

--- lesson_02/script.sh
+++ lesson_02/script.sh
@@ -26,9 +26,9 @@ function output_log {
 }
 
 function init {
-    yum update -y
+#    yum update -y
     yum install -y mdadm smartmontools hdparm gdisk
-    yum install -y nano wget tree
+    yum install -y nano wget tree mc
 
     touch $LOG_FILE
     output_log
@@ -41,8 +41,7 @@ function raid {
             mdadm --create --verbose /dev/md$1 --force --level=0 --raid-devices=2 /dev/sd{b,c}
             ;;
         1)  echo "Creating RAID 1"
-            mdadm --create --verbose --metadata=1.2 /dev/md$1 --force --level=1 --raid-devices=2 /dev/sd{b,c}
-            mdadm /dev/md$1 --add /dev/sdd
+            mdadm --create --verbose --metadata=1.2 /dev/md$1 --force --level=1 --raid-devices=3 /dev/sd{b,c,d}
             mdadm /dev/md$1 --add /dev/sde
             ;;
         5)  echo "Creating RAID 5"
@@ -61,16 +60,16 @@ function raid {
 
     parted -s /dev/md$1 mklabel gpt
 
-    parted /dev/md$1 mkpart primary ext4 0% 20%
-    parted /dev/md$1 mkpart primary ext4 20% 40%
-    parted /dev/md$1 mkpart primary ext4 40% 60%
-    parted /dev/md$1 mkpart primary ext4 60% 80%
-    parted /dev/md$1 mkpart primary ext4 80% 100%
+    parted /dev/md$1 mkpart primary ext4 0% 5%
+    parted /dev/md$1 mkpart primary ext4 5% 10%
+    parted /dev/md$1 mkpart primary ext4 10% 25%
+    parted /dev/md$1 mkpart primary ext4 25% 50%
+    parted /dev/md$1 mkpart primary ext4 50% 100%
 
     for i in $(seq 1 5); do
         mkdir -p /mnt/raid/md$1p$i
-        mkfs.ext4 /dev/md$1p$i;
-        mount /dev/md$1p$i /mnt/raid/md$1p$i;
+        mkfs.ext4 /dev/md$1p$i
+        mount /dev/md$1p$i /mnt/raid/md$1p$i
     done
 
     # Создание файла конфигурации mdadm.conf
@@ -82,8 +81,40 @@ function raid {
 
 # Подготовка "живой" системы к переносу на RAID
 function transfer_to_raid {
-    return
+
+    # Удалеям ошметки предыдущего задания
+    for i in $(seq 1 5); do
+        umount /dev/md$1p$i
+        rmdir /mnt/raid/md$1p$i
+    done
+    rmdir /mnt/raid
+
+    mkfs.ext4   /dev/md$1p1
+    mkfs.ext4   /dev/md$1p2
+    mkswap      /dev/md$1p3
+    mkfs.ext4   /dev/md$1p4
+    mkfs.ext4   /dev/md$1p5
+
+    mkdir -p /mnt/{boot,home,var,root_dir}
+    mount /dev/md$1p1 /mnt/boot
+    mount /dev/md$1p2 /mnt/home
+    mount /dev/md$1p4 /mnt/var
+    mount /dev/md$1p5 /mnt/root_dir
+
+    # Копируем рабочую систему в /mnt.
+    rsync -axu /boot /mnt/boot/
+    rsync -axu /home /mnt/home/
+    rsync -axu /var /mnt/var/
+    rsync -axu --recursive --exclude /vagrant --exclude /boot --exclude /home --exclude /var --exclude swapfile / /mnt/root_dir/
+
+    # Монтируем служебные файловые системы в /mnt и CHROOTимся в новый корень.
+    mount --bind /proc /mnt/root_dir/proc &&    \
+    mount --bind /dev /mnt/root_dir/dev &&      \
+    mount --bind /sys /mnt/root_dir/sys &&      \
+    mount --bind /run /mnt/root_dir/run &&      \
+    chroot /mnt/root_dir/
 }
 
 init
-raid 1
+raid 6
+transfer_to_raid 6
