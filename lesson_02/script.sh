9692886ac70290ba17b1121d8a31dd6737c44cd7
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-08-15 01:17:56 +0300

    Система переносится успешно

--- lesson_02/script.sh
+++ lesson_02/script.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 LOG_FILE=report.log
-OUTFILE=continued_transfer.sh
+OUTFILE=finish.sh
 DISKS=("/dev/sdb" "/dev/sdc" "/dev/sdd" "/dev/sde")
+RAID_LEVEL=5
 
 # Логирование вывода команд для последующего составления отчета в README.md
 function output_log {
@@ -13,31 +14,19 @@ function output_log {
     done
 }
 
-function prepare_system {
-#    yum update -y
-    yum install -y mdadm smartmontools hdparm gdisk
-    #yum install -y epel-release
-    yum install -y nano wget tree #haveged
-
-    touch $LOG_FILE
-    output_log
-}
-
-# Подготовка дисков
+# Разметка дисков
 function prepare_disks {
-
     for i in ${DISKS[@]}; do
         parted -s $i mktable gpt
         parted -s $i mkpart primary 2048s 4096s   # GPT-раздел
         parted -s $i set 1 bios_grub on
         parted -s $i mkpart primary ext4 4M 100%  # раздел для RAID
     done
-
     output_log
 }
 
-# Создание RAID уровней 0/1/5/6/10 для тестирования
-function raid {
+# Создание RAID одного из уровней 0/1/5/6/10
+function create_raid {
 
     case $1 in
         0)  echo "Creating RAID 0"
@@ -61,6 +50,7 @@ function raid {
             ;;
     esac
 
+    # Создание разделов на RAID`е
     parted -s /dev/md$1 mktable gpt
 #    parted -s /dev/md$1 mkpart primary 2048s 4096s      #GPT-раздел
 #    parted -s /dev/md$1 set 1 bios_grub on
@@ -70,7 +60,8 @@ function raid {
     parted -s /dev/md$1 mkpart primary ext4 25% 50%     #раздел №4
     parted -s /dev/md$1 mkpart primary ext4 50% 100%    #раздел №5
 
-   for i in $(seq 1 5); do
+    # Монтирование разделов
+    for i in $(seq 1 5); do
         mkdir -p /mnt/raid/md$1p$i
         mkfs.ext4 /dev/md$1p$i
         mount /dev/md$1p$i /mnt/raid/md$1p$i
@@ -83,95 +74,62 @@ function raid {
     output_log
 }
 
-# Подготовка "живой" системы к переносу на RAID
+# Подготовка к переносу "живой" системы с одним разделом
+# на RAID с несколькими разделами
 function transfer_to_raid {
 
-    # Удаляем ошметки предыдущего задания
+    # Размонтирование разделов и удаление ненужных директорий,
+    # оставшихся при выполнении предыдущего задания
     for i in $(seq 1 5); do
         umount /dev/md$1p$i
         rmdir /mnt/raid/md$1p$i
     done
     rmdir /mnt/raid
 
-#    mkfs.ext4   /dev/md$1p1
-#    mkfs.ext4   /dev/md$1p2
-#    mkswap      /dev/md$1p3
-#    mkfs.ext4   /dev/md$1p4
-#    mkfs.ext4   /dev/md$1p5
-
-    parted -s /dev/md$1 mktable gpt
-#    parted -s /dev/md$1 mkpart primary ext4 0% 100%
-
-    mkfs.ext4   /dev/md$1
+    # Перенос системы на RAID
+    mkfs.ext4 /dev/md$1
     mount /dev/md$1 /mnt/
+    rsync -axu --progress --exclude /swapfile --exclude /vagrant / /mnt/
+}
 
-#    mkdir -p /mnt/{boot,home,var}
-#    mount /dev/md$1p1 /mnt/boot
-#    mount /dev/md$1p2 /mnt/home
-#    mount /dev/md$1p4 /mnt/var
-#    mount /dev/md$1p5 /mnt/
-
-    # Копируем рабочую систему в /mnt.
-#    rsync -axu /boot/ /mnt/boot/
-#    rsync -axu /home/ /mnt/home/
-#    rsync -axu /var/ /mnt/var/
-#    rsync   -axu --recursive --progress \
-#            --exclude /vagrant \
-#            --exclude /boot \
-#            --exclude /home \
-#            --exclude /var \
-#            --exclude swapfile / /mnt/
-
-    rsync -axu --progress --exclude /vagrant / /mnt/
-
-    #
-    # Формирование скрипта, который необходимо запустить вручную после
-    # развертывания тестового окружения
+# Формирование сценария, который необходимо запустить вручную после
+# развертывания тестового окружения
+function gen_finish_script {
     (
     cat << '_EOF_'
 #!/usr/bin/env bash
+
 mount --bind /proc /mnt/proc
 mount --bind /dev /mnt/dev
 mount --bind /sys /mnt/sys
 mount --bind /run /mnt/run
 chroot /mnt/ /bin/bash <<'EOT'
-# Создание нового /etc/fstab
-echo "# My scripted /etc/fstab" > /etc/fstab
-#echo -n `blkid |grep md1p1 | cut -d" " -f 2`  >> /etc/fstab
-#echo '  /boot   ext4    defaults         0       0' >> /etc/fstab
-#echo -n `blkid |grep md1p2 | cut -d" " -f 2`  >> /etc/fstab
-#echo '  /home   ext4    defaults         0       0' >> /etc/fstab
-#echo -n `blkid |grep md1p3 | cut -d" " -f 2`  >> /etc/fstab
-#echo '  /swap   swap    defaults         0       0' >> /etc/fstab
-#echo -n `blkid |grep md1p4 | cut -d" " -f 2`  >> /etc/fstab
-#echo '  /var    ext4    defaults         0       0' >> /etc/fstab
-#echo -n `blkid |grep md1p5 | cut -d" " -f 2`  >> /etc/fstab
-#echo '  /       ext4    defaults         0       0' >> /etc/fstab
+# /etc/fstab
+RAID_NAME=`cat /proc/mdstat |grep md | cut -d" " -f1`
 (
-echo -n `blkid |grep md1 | cut -d" " -f 2`
-echo '  /   ext4    defaults         0       0'
+echo -n `blkid |grep $RAID_NAME | cut -d" " -f 2`
+echo '  /   ext4    defaults         0 0'
 ) >> /etc/fstab
-# Создание файла конфигурации mdadm.conf
+# mdadm.conf
 echo "DEVICE partitions" > /etc/mdadm.conf
 mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm.conf
 sed -i 's/SELINUX=.*/SELINUX=permissive/' /etc/selinux/config
 sed -i 's/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="no_timer_check console=tty0 console=ttyS0,115200n8 net.ifnames=0 biosdevname=0 elevator=noop crashkernel=auto rd.auto=1"/' /etc/default/grub
 dracut --force /boot/initramfs-$(uname -r).img $(uname -r)
 grub2-mkconfig -o /boot/grub2/grub.cfg && \
-grub2-install /dev/sdb && \
-grub2-install /dev/sdc && \
-grub2-install /dev/sdd && \
-grub2-install /dev/sde
+grub2-install /dev/sdb && grub2-install /dev/sdc
 EOT
 
 _EOF_
-    ) > $OUTFILE
-    chmod +x $OUTFILE
+    ) > $1
+    chmod +x $1
 
 }
 
 
-prepare_system
-prepare_disks
-raid 1
-transfer_to_raid 1
+yum install -y mdadm smartmontools hdparm gdisk nano tree
+touch $LOG_FILE
+output_log
+
+prepare_disks && create_raid $RAID_LEVEL
+transfer_to_raid $RAID_LEVEL && gen_finish_script $OUTFILE
