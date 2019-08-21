152de143f29b86027065078b53582b8591a1d6ee
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-08-21 22:36:04 +0300

    A little changes

--- lesson_03/script.sh
+++ lesson_03/script.sh
@@ -4,3 +4,13 @@ function reduce_size_root(parameter) {
     #statements
     return
 }
+
+function new_volume(parameter) {
+    #statements
+    return
+}
+
+function set_mirror(parameter) {
+    #statements
+    return
+}
