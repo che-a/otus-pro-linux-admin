41880c54e4a6ebb645f5398d0dddd2374d62f1b7
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-09-01 18:48:58 +0300

    Update

--- lesson_03/script.sh
+++ lesson_03/script.sh
@@ -1,31 +1,36 @@
 #!/usr/bin/env bash
 
-function reduce_size_root(parameter) {
+function reduce_size_root() {
     #statements
     return
 }
 
-function new_volume(parameter) {
+function new_volume() {
     #statements
     return
 }
 
-function set_mirror(parameter) {
+function set_mirror() {
     #statements
     return
 }
 
-function new_volume(parameter) {
+function new_volume() {
     #statements
     return
 }
 
-function gen_files(parameter) {
+function gen_files() {
     #statements
     return
 }
 
-function gen_files2(parameter) {
+function gen_files2() {
     #statements
     return
 }
+
+mkdir -p ~root/.ssh
+cp ~vagrant/.ssh/auth* ~root/.ssh
+# yum update -y
+yum install -y mdadm smartmontools hdparm gdisk
