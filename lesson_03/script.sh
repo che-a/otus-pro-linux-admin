5205ba8981e2af907503cb3fd2c7f937e803febc
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-09-01 19:05:23 +0300

    Update script.sh

--- lesson_03/script.sh
+++ lesson_03/script.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 
+LOG_FILE=report.log # Файл, в который будет записано подробное логирование выполнения всех заданий
+REDUCE_TO_SIZE=8    # Размер тома, до которого его нужно уменьшить
+
+# Логирование вывода информационных команд с целью отслеживания изменений
+# в дисковой подсистеме по мере выполнения заданий
+function output_log {
+    local CMDS=('lsblk' \
+                'df -h -x tmpfs -x devtmpfs' \
+                'blkid')
+
+    for i in $(seq 1 80); do echo -n "*" >> $LOG_FILE; done
+    (echo ; echo "**** $1:") >> $LOG_FILE
+    for i in $(seq 1 80); do echo -n "*" >> $LOG_FILE; done
+    echo >> $LOG_FILE
+
+    for i in "${CMDS[@]}"; do
+        (echo ==== $i ====; sh -c "$i") >> $LOG_FILE;
+    done
+}
+
+# Уменьшение размера тома с корневой файловой системой формата XFS
 function reduce_size_root() {
-    #statements
+    #
     return
 }
 
@@ -34,3 +55,6 @@ mkdir -p ~root/.ssh
 cp ~vagrant/.ssh/auth* ~root/.ssh
 # yum update -y
 yum install -y mdadm smartmontools hdparm gdisk
+yum install -y xfsdump
+
+touch $LOG_FILE; output_log "Исходная система"
