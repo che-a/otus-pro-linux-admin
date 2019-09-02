f773e73f67a95aec83120bdc30c938f2d27b0348
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-09-02 14:24:59 +0300

    Update

--- lesson_03/lvm.sh
+++ lesson_03/lvm.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
-# Файл, в который будет логироваться ход выполнения всех заданий
-REPORT_LOG=report_lvm.log
-REDUCE_TO_SIZE=8        # Новый размер тома, ГБ
-TMP_PV='/dev/sdb'       # Временный физческий том
-TMP_VG='VG01'           #
-TMP_LV='lv_tmp_root'    #
+# Домашнее задание №3
+# STAGE 0       Первый запуск сценария, создание временного тома,
+#               настройка загрузки с нового тома, перезагрузка
+# STAGE 1       Загрузка с временного тома, удаление старого раздела,
+#               создание нового раздела нужного размера
+
+REPORT_LOG=report_lvm.log   # Файл, в который будет логироваться ход выполнения всех заданий
+STAGE_LOG=/home/vagrant/stage.log         # Файл, в который записывается уровенеь проведенных изменений, чтобы после перезагрузки системы не начинать все сначала
+REDUCE_TO_SIZE=8G           # Новый размер тома, ГБ
+TMP_PV='/dev/sdb'           # Временный физческий том
+TMP_VG='VG01'               #
+TMP_LV='lv_tmp_root'        #
+CUR_PV=
+CUR_VG=
+CUR_LV=
+NEW_PV=
+NEW_VG=
+NEW_LV=
+STAGE=
 
 # Логирование вывода информационных команд с целью отслеживания изменений
 # по ходу выполнения задания
@@ -42,40 +55,46 @@ function reduce_size_root() {
     xfsdump -J - /dev/VolGroup00/LogVol00 | xfsrestore -J - /mnt
 
     # Переконфигурирование grub, чтобы после рестарта временный том был корнем ФС
-    for i in /proc/ /sys/ /dev/ /run/ /boot/; do mount --bind $i /mnt/$i; done
-    # chroot /mnt/
-    # grub2-mkconfig -o /boot/grub2/grub.cfg
-    # Обновим образ initrd
-    # cd /boot ; for i in `ls initramfs-*img`; do dracut -v $i `echo $i|sed "s/initramfs-//g;s/.img//g"` --force; done
-}
+    for i in /proc/ /sys/ /dev/ /run/ /boot/; do
+        mount --bind $i /mnt/$i;
+    done
+chroot /mnt/ /bin/bash <<'EOT'
+grub2-mkconfig -o /boot/grub2/grub.cfg
+# Обновление образа initrd
+cd /boot;
+for i in `ls initramfs-*img`; do
+    dracut -v $i `echo $i|sed "s/initramfs-//g;s/.img//g"` --force;
+done
+EOT
+    sed -i 's/rd.lvm.lv=VolGroup00/LogVol00 =.*/rd.lvm.lv=VG01/lv_tmp_root/' /boot/grub2/grub.cfg
+    # reboot
+    #
 
-function new_volume() {
-    #statements
-    return
-}
-
-function set_mirror() {
-    #statements
-    return
-}
-
-function new_volume() {
-    #statements
-    return
-}
-
-function gen_files() {
-    #statements
-    return
-}
-
-function gen_files2() {
-    #statements
-    return
+    #
 }
 
 
-touch $REPORT_LOG; output_log "Исходная система"
+#touch $REPORT_LOG; output_log "Исходная система"
 
-reduce_size_root
-output_log "Уменьшение тома с корневой ФС"
+#reduce_size_root
+#output_log "Уменьшение тома с корневой ФС"
+
+
+if [ ! -f $STAGE_LOG ]; then
+   echo "Файл '$STAGE_LOG' не существует. Выполнять нечего."
+   exit 1
+else
+    STAGE=`cat $STAGE_LOG`
+
+    case $STAGE in
+        0)  echo "Текущий уровень: 0"
+            ;;
+        1)  echo "Текущий уровень: 1"
+            ;;
+        2)  echo "Текущий уровень: 2"
+            ;;
+        *)  echo "Ошибка в файле $STAGE_LOG"
+            exit 1
+            ;;
+    esac
+fi
