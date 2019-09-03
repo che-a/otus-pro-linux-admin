e9deed953297f67b0b57369efadae8da31ab0669
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-09-03 11:43:59 +0300

    Update

--- lesson_03/lvm_reduce_root.sh
+++ lesson_03/lvm_reduce_root.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-NEW_SIZE=8G
-
 VG='VolGroup00'
 LV='LogVol00'
 
 TMP_PV='/dev/sdb'
 TMP_VG='VGTMP'
 TMP_LV='lv_tmp'
+
+NEW_SIZE=8G
 
 # Создание временного тома для корня и копирование в него данных из текущего
 function lvm_create_tmp_root {
@@ -17,6 +17,7 @@ function lvm_create_tmp_root {
     mkfs.xfs /dev/$TMP_VG/$TMP_LV
 }
 
+# Перенос файлов корня на временный том
 function lvm_move_to_tmp_root {
     mount /dev/$TMP_VG/$TMP_LV /mnt
     xfsdump -J - /dev/$VG/$LV | xfsrestore -J - /mnt
@@ -41,12 +42,12 @@ EOT
 # Удаление старого корневого тома и создание нового
 function lvm_create_new_root {
     lvremove /dev/$VG/$LV --force
-    lvcreate -y -n $NEW_LV -L $NEW_SIZE /dev/$VG
-    mkfs.xfs /dev/$VG/$NEW_LV
+    lvcreate -y -n $LV -L $NEW_SIZE /dev/$VG
+    mkfs.xfs /dev/$VG/$LV
 }
 
 function lvm_move_to_new_root {
-    mount /dev/$VG/$NEW_LV /mnt
+    mount /dev/$VG/$LV /mnt
     xfsdump -J - /dev/$TMP_VG/$TMP_LV | xfsrestore -J - /mnt
 }
 
@@ -55,3 +56,31 @@ function lvm_del_tmp_root {
     vgremove /dev/$TMP_VG
     pvremove $TMP_PV
 }
+
+STAGE=`cat stage.log |grep reduce_root | cut -d "=" -f 2`
+case $STAGE in
+    0)  sed -i 's/reduce_root=.*/reduce_root=1/' stage.log
+        lvm_create_tmp_root
+        lvm_move_to_tmp_root
+        lvm_reconfig_grub2 $VG $LV $TMP_VG $TMP_LV
+        reboot
+        ;;
+
+    1)  sed -i 's/reduce_root=.*/reduce_root=2/' stage.log
+        lvm_create_new_root
+        lvm_move_to_new_root
+        lvm_reconfig_grub2 $VG $LV $VG $LV
+        reboot
+        ;;
+
+    2)  sed -i 's/reduce_root=.*/reduce_root=3/' stage.log
+        lvm_del_tmp_root
+        ;;
+
+    3)  echo "Корневой том уменьшен. Операции завершены"
+        ;;
+
+    *)  echo "Ошибка в файле $STAGE_LOG"
+        exit 1
+        ;;
+esac
