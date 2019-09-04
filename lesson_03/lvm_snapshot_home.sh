538a0d271b4f33663e0bf1efdf8220cf68543788
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-09-04 22:02:23 +0300

    Update

--- lesson_03/lvm_snapshot_home.sh
+++ lesson_03/lvm_snapshot_home.sh
@@ -3,16 +3,19 @@
 VG='VolGroup00'
 LV='lv_home'
 SIZE='100M'
-SNAPSHOT='home_snap'
+SNAP_NAME='home_snap'
 
-touch /home/file{1..20}
+for i in $(seq 1 20);do
+    touch /home/file$i
+    echo "OTUS. Адинистратор Linux" >> /home/file$i
+done
 
-lvcreate -L $SIZE -s -n $SNAPSHOT /dev/$VG/$LV
+lvcreate -L $SIZE -s -n $SNAP_NAME /dev/$VG/$LV
 
 rm -f /home/file{11..20}
 
 umount /home
 
-lvconvert --merge /dev/$VG/$SNAPSHOT
+lvconvert --merge /dev/$VG/$SNAP_NAME
 
 mount /home
