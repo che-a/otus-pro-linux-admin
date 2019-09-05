2559a03866046b3905c2b353038567dc20d4ac51
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-09-06 00:38:46 +0300

    ZFS кэш и снапшоты работают

--- lesson_03/zfs.sh
+++ lesson_03/zfs.sh
@@ -1,32 +1,69 @@
 #!/usr/bin/env bash
 
-STAGE_LOG_FILE='stage.log'
-STAGE_LOG_PATH='/home/vagrant/'$STAGE_LOG_FILE
+SERVICE_FILE='zfs_otus.service'
+SERVICE_PATH='/etc/systemd/system/'$SERVICE_FILE
 
-CENTOS_MINOR_VER=`cat /etc/redhat-release | cut -d" " -f 4 | cut -d"." -f 2`
-ZFS_PATH="http://download.zfsonlinux.org/epel"
-ZFS_PACKAGE="$ZFS_PATH/zfs-release.el7_$CENTOS_MINOR_VER.noarch.rpm"
+STAGE_FILE='stage.log'
+STAGE_PATH='/home/vagrant/'$STAGE_FILE
+
+# Автозапуск данного сценария после перезагрузки системы
+function make_systemd_service {
+    touch $SERVICE_PATH
+    chmod 664 $SERVICE_PATH
+
+cat > $SERVICE_PATH <<'_EOF_'
+[Unit]
+Description=Using ZFS
+After=network.target
+[Service]
+Type=oneshot
+User=root
+ExecStart=/home/vagrant/zfs.sh
+[Install]
+WantedBy=multi-user.target
+_EOF_
+
+    systemctl enable $SERVICE_FILE
+}
 
 
-STAGE=`cat $STAGE_LOG_PATH |grep lvm_reduce_move | cut -d "=" -f 2`
+STAGE=`cat $STAGE_PATH |grep zfs | cut -d "=" -f 2`
 case $STAGE in
-    0)  yum install -y update
-        yum install -y $ZFS_PACKAGE
-        yum install -y kernel-devel zfs
-        sed -i 's/zfs=.*/zfs=1/' $STAGE_LOG_PATH
+    0)  make_systemd_service
+        yum update -y
+        sed -i 's/zfs=.*/zfs=1/' $STAGE_PATH
         reboot
         ;;
 
-    1)  sed -i 's/zfs=.*/zfs=2/' $STAGE_LOG_PATH
+    1)  CENTOS7_MINOR_VER=`cat /etc/redhat-release | cut -d" " -f 4 | cut -d"." -f 2`
+        ZFS_REPO_PATH="http://download.zfsonlinux.org/epel"
+        ZFS_REPO="$ZFS_REPO_PATH/zfs-release.el7_$CENTOS7_MINOR_VER.noarch.rpm"
+        yum install -y $ZFS_REPO
+        # kABI-tracking kmod
+        sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/zfs.repo
+        sed -i '0,/enabled=1/{s/enabled=1/enabled=0/}' /etc/yum.repos.d/zfs.repo
+        yum install -y zfs
+
+        zpool create otus_pool /dev/sdb /dev/sdc -f -m none
+        zpool add otus_pool cache /dev/sdd
+        zpool add otus_pool log /dev/sde
+        zfs create otus_pool/opt
+        zfs set mountpoint=/opt otus_pool/opt
+
+        # Делаем снимок состояния
+        echo "Version 1" > /opt/test.txt
+        zfs snapshot otus_pool/opt@version1
+
+        systemctl disable $SERVICE_FILE
+        sed -i 's/zfs=.*/zfs=2/' $STAGE_PATH
+        # Перзагрузка для проверки работоспособности системы
+        reboot
         ;;
 
-    2)  sed -i 's/zfs=.*/zfs=3/' $STAGE_LOG_PATH
+    2)  echo "Все операции с ZFS завершены."
         ;;
 
-    3)  echo "Все операции завершены."
-        ;;
-
-    *)  echo "Ошибка в файле $STAGE_LOG"
+    *)  echo "Ошибка в файле $STAGE_FILE"
         exit 1
         ;;
 esac
