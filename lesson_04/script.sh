2ff5b6c4cce16f5e1b85fbf21d24a15a4ca6d9d6
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-09-10 23:26:34 +0300

    Update

--- lesson_04/script.sh
+++ lesson_04/script.sh
@@ -11,26 +11,6 @@ ALL_IP_COUNT=`echo ${#ALL_IP[@]}`
 declare -A X_IP
 
 function get_x_ip {
-    #echo "" > $X_IP_FILE
-
-    #while read LINE; do
-    #    IP=`echo $LINE | cut -d' ' -f1`
-#        #echo $LINE | cut -d' ' -f1
-#        echo $IP
-#
-#    done < $LOG_FILE
-
-
-    local count=1
-
-    for i in ${ALL_IP[@]}; do
-        X_IP[$i]=0
-        echo "$count. $i : ${X_IP[$i]}"
-        count=$((count+1))
-    done
-
-    echo "Всего IP-адресов: "$ALL_IP_COUNT
-
 
 }
 
