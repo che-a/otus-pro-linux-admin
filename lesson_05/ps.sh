ae5f777933e959e1d3c1153b8088cde0b44dc1f8
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-11-08 00:38:49 +0300

    Update ps.sh

--- lesson_05/ps.sh
+++ lesson_05/ps.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PROGNAME=`basename $0`
+#
 # Массив PID заполняем идентификаторами процессов из каталога /proc/
 PIDS=(`ls /proc/ | grep "^[0-9]" | sort -n`)
 TTY=
@@ -8,37 +9,77 @@ STAT=
 TIME=
 CMD=
 
+# Функция записывает в переменную TTY название связанного с процессом терминала
+# Параметры: $1 -- PID процесса
 function get_tty {
+    # TTY_NR -- это поле tty_nr в файле /proc/[pid]/stat
+    # Согласно документации:
+    #   The minor device number is contained in the combination of bits 31 to 20
+    #   and 7 to 0; the major device number is in bits 15 to 8.
+    # В этой программе биты с 31-го по 20-й не рассматриваются (для упрощения).
+    # Далее идет преобразование в двоичную форму представления числа, разделение
+    #   этого числа на мажорную и минорную части и преобразование каждой из них
+    #   в десятичную форму для дальнейшего использования.
+    local TTY_NR=`cat /proc/$1/stat | gawk '{print $7}'`
+    local BIN=$( printf "%016d" `echo "obase=2;$TTY_NR" | bc` )
+    local BIN_MAJOR=`echo $BIN | gawk '{print substr ($0, 0, 8)}'`
+    local BIN_MINOR=`echo $BIN | gawk '{print substr ($0, 9, 16)}'`
+    local DEC_MAJOR=`echo "ibase=2;$BIN_MAJOR" | bc`
+    local DEC_MINOR=`echo "ibase=2;$BIN_MINOR" | bc`
 
-    return
+    # Определение названия терминала.
+    # Данные для выбора взяты из файла /proc/devices
+
+    case $DEC_MAJOR in
+        4)      TTY='tty/'$DEC_MINOR
+                ;;
+        128)    TTY='ptm/'$DEC_MINOR
+                ;;
+        136)    TTY='pts/'$DEC_MINOR
+                ;;
+        *)      TTY='?'
+                ;;
+    esac
+}
+
+function get_stat {
+    STAT=`cat /proc/$1/status | grep State | gawk '{print $2}'`
+    local flag_hp=      # high-priority (not nice to other users)
+    local flag_N=       # low-priority (nice to other users)
+    local flag_L=       # has pages locked into memory (for real-time and custom IO)
+    local flag_s=       # is a session leader
+    local flag_l=`cat /proc/$1/status | grep Threads | gawk '{print $2}'`       # is multi-threaded (using CLONE_THREAD, like NPTL pthreads do)
+    local flag_plus=    # is in the foreground process group
+
+    if [ $flag_l -gt 1 ]; then
+        STAT=$STAT"l"
+    fi
+}
+
+function get_time {
+    TIME='X:XX'
+}
+
+function get_command {
+    CMD=`cat /proc/$1/cmdline | tr '\0' ' '`
+    if [[ -z $CMD ]]; then
+        CMD='['`cat /proc/$1/status | grep Name | gawk '{print $2}'`']'
+    else
+        CMD=`echo $CMD | awk '{print substr ($0, 0, 60)}'`
+    fi
 }
 
 function ps_ax {
 
-
     echo "  PID TTY      STAT   TIME COMMAND"
 
-    # Последовательно перебираем массив с идентификаторами процессов
     for PID in ${PIDS[@]}; do
-        # На момент перебора некоторых процесов уже может не существовать
+        # На момент перебора некоторых процессов уже может не существовать
         if [ -d "/proc/$PID" ]; then
-            # Колонка TTY
-            get_tty
-            #TTY=`cat /proc/$PID/stat | gawk '{print $7}'`
-            #if [ $TTY == 0 ]; then
-            #    TTY='?'
-            #fi
-            # Колонка STAT
-            STAT=`cat /proc/$PID/status | grep State | gawk '{print $2}'`
-            # Колонка TIME
-            TIME='X:XX'
-            # Колонка CMD
-            CMD=`cat /proc/$PID/cmdline | tr '\0' ' '`
-            if [[ -z $CMD ]]; then
-                CMD='['`cat /proc/$PID/status | grep Name | gawk '{print $2}'`']'
-            else
-                CMD=`echo $CMD | awk '{print substr ($0, 0, 60)}'`
-            fi
+            get_tty $PID        # Поле TTY
+            get_stat $PID       # Поле STAT
+            get_time $PID       # Поле TIME
+            get_command $PID    # Поле CMD
 
             # Команда printf некорректно выводит заданное число символов строки,
             # которые содержат пробелы
