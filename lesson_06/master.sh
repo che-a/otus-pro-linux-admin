d625d780268f783d9f80a07ed56572a41a1db83d
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-11-11 02:14:28 +0300

    Update

--- lesson_06/master.sh
+++ lesson_06/master.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 NGINX_SRPM='nginx-1.16.1-1.el7.ngx.src.rpm'
+NGINX_RPM='nginx-1.16.1-1.el7.ngx.x86-64.rpm'
 OPENSSL=
 
 cd /root/
@@ -17,3 +18,10 @@ yum-builddep -y rpmbuild/SPECS/nginx.spec
 
 # Правка SPEC
 sed -i '116i --with-openssl=/root/'$OPENSSL' \\' /root/rpmbuild/SPECS/nginx.spec
+
+rpmbuild -bb rpmbuild/SPECS/nginx.spec
+
+yum localinstall -y rpmbuild/RPMS/x86_64/$NGINX_RPM
+
+systemctl start nginx
+systemctl status nginx
