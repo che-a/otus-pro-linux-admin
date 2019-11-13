39102139eb87d2d8fcb99ca0867cf1cf23e981af
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-11-13 22:32:53 +0300

    Delete "InexIgnore repodata" from Apache config

--- lesson_06/srv.sh
+++ lesson_06/srv.sh
@@ -58,7 +58,6 @@ function customize_apache
             echo '        Options Indexes Includes FollowSymLinks'
             echo '        IndexOptions FancyIndexing FoldersFirst IconsAreLinks NameWidth=60'
             echo '        IndexIgnore header.html'
-            echo '        IndexIgnore repodata'
             echo '        HeaderName header.html'
             echo '    </Directory>'
             echo "    ErrorLog $DOC_DIR/$REPO/log/error.log"
