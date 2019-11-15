742bbb40be300b4e371fff8a891b535a057c8e03
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-11-16 01:22:12 +0300

    Fix scripts

--- lesson_07/vg_rename.sh
+++ lesson_07/vg_rename.sh
@@ -3,50 +3,13 @@
 VG_OLD=`vgdisplay | grep "VG Name" | gawk '{print $3}'`
 VG_NEW='CheLes07Root'
 
-REPORT_FILE='/home/vagrant/report.log'
-UNIT_FILE='/etc/systemd/system/vg_rename.service'
 
+vgrename $VG_OLD $VG_NEW
 
-function make_systemd_service
-{
-    touch $UNIT_FILE
-    chmod 664 $UNIT_FILE
+sed -i "s/$VG_OLD/$VG_NEW/g" /etc/fstab
+sed -i "s/$VG_OLD/$VG_NEW/g" /boot/grub2/grub.cfg
 
-cat > $UNIT_FILE <<'_EOF_'
-[Unit]
-Description=Volume Group Rename
-After=network.target
-[Service]
-Type=oneshot
-User=root
-ExecStart=/home/vagrant/vg_rename.sh
-[Install]
-WantedBy=multi-user.target
-_EOF_
+dracut -f -v
 
-    systemctl enable $UNIT_FILE
-}
-
-function report
-{
-    (
-        echo "========== $1 =========="
-        vgs; echo
-    ) >> $REPORT_FILE
-}
-
-
-if [ -s $REPORT_FILE ]; then
-    systemctl disable vg_rename.service
-
-    report 'AFTER RENAMING VG'
-else
-    make_systemd_service
-    report 'BEFORE RENAMING VG'
-    vgrename $VG_OLD $VG_NEW
-
-    sed -i "s/$VG_OLD/$VG_NEW/g" /etc/fstab
-    sed -i "s/$VG_OLD/$VG_NEW/g" /boot/grub2/grub.cfg
-
-    dracut -f -v
-fi
+echo "Reboot virtual machine ..."
+reboot
