552d2cccddcec8826c69bee5517a4db014a5cdfe
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-11-17 22:43:33 +0300

    Грохнул всю дичь

--- lesson_08/script.sh
+++ lesson_08/script.sh
@@ -24,51 +24,21 @@ function task2
     systemctl start spawn-fcgi
 }
 
-function task3v1
+function task3
 {
-    echo "127.0.0.1 site1.otus site2.otus" >> "/etc/hosts"
+    cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd-inst1.conf
+    cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd-inst2.conf
 
-    # Копируем юнит из шаблона
-    cp /usr/lib/systemd/system/httpd.service /etc/systemd/system/httpd@.service
-    # Добавляем в шаблон параметр для запуска нескольких экземпляров
-    sed -i '/^EnvironmentFile/ s/$/-%I/' /etc/systemd/system/httpd@.service
+    sed -i 's/Listen 80/Listen 8080/' /etc/httpd/conf/httpd-inst2.conf
+    sed -i '/ServerRoot "\/etc\/httpd"/a PidFile \/var\/run\/httpd-inst2.pid' \
+        /etc/httpd/conf/httpd-inst2.conf
 
-    # Che
-    echo 'IncludeOptional sites-enabled/*.conf' >> "/etc/httpd/conf/httpd-instance1.conf"
-    echo 'IncludeOptional sites-enabled/*.conf' >> "/etc/httpd/conf/httpd-instance2.conf"
-
-
-
-    # Создаем файлы конфигурации для каждого экземпляра веб-сервера
-    cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd-instance1.conf
-    cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd-instance2.conf
-    mv /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak
-
-    sed -i 's/Listen 80/Listen 8081/' /etc/httpd/conf/httpd-instance1.conf
-    sed -i 's/Listen 80/Listen 8082/' /etc/httpd/conf/httpd-instance2.conf
-
-    # Che
-    ln -s "/etc/httpd/sites-available/site1.otus.conf" "/etc/httpd/sites-enabled/site1.otus.conf"
-    ln -s "/etc/httpd/sites-available/site2.otus.conf" "/etc/httpd/sites-enabled/site2.otus.conf"
-
-    rm "/etc/httpd/conf.d/welcome.conf"
-
-    sed -i "s/SELINUX=.*/SELINUX=permissive/" /etc/selinux/config
-    reboot
-
-
-#sed -i '/ServerRoot "\/etc\/httpd"/a PidFile \/var\/run\/httpd-second.pid' /etc/httpd/conf/httpd-second.conf
-
-#systemctl disable httpd
-#systemctl daemon-reload
-#systemctl start httpd@first
-#systemctl start httpd@second
-
+    systemctl daemon-reload
+    systemctl start httpd@inst{1,2}
 }
-
 
 yum install -y mc nano tree
 
 task1
 task2
-task3v1
+task3
