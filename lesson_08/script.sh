fe3aadf185276ebb5cb93e4966eb721431880abe
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-11-17 21:33:06 +0300

    Какая-то дичь

--- lesson_08/script.sh
+++ lesson_08/script.sh
@@ -26,19 +26,35 @@ function task2
 
 function task3v1
 {
-# Копируем юнит из шаблона
-cp /usr/lib/systemd/system/httpd.service /etc/systemd/system/httpd@.service
-# Добавляем параметр для запуска нескольких экземпляров
-sed -i '/^EnvironmentFile/ s/$/-%I/' /etc/systemd/system/httpd@.service
+    echo "127.0.0.1 site1.otus site2.otus" >> "/etc/hosts"
+
+    # Копируем юнит из шаблона
+    cp /usr/lib/systemd/system/httpd.service /etc/systemd/system/httpd@.service
+    # Добавляем в шаблон параметр для запуска нескольких экземпляров
+    sed -i '/^EnvironmentFile/ s/$/-%I/' /etc/systemd/system/httpd@.service
+
+    # Che
+    echo 'IncludeOptional sites-enabled/*.conf' >> "/etc/httpd/conf/httpd-instance1.conf"
+    echo 'IncludeOptional sites-enabled/*.conf' >> "/etc/httpd/conf/httpd-instance2.conf"
 
 
-cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd1.conf
-cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd2.conf
-mv /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak
 
-sed -i 's/Listen 80/Listen 8081/' /etc/httpd/conf/httpd1.conf
-sed -i 's/Listen 80/Listen 8082/' /etc/httpd/conf/httpd2.conf
+    # Создаем файлы конфигурации для каждого экземпляра веб-сервера
+    cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd-instance1.conf
+    cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd-instance2.conf
+    mv /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak
 
+    sed -i 's/Listen 80/Listen 8081/' /etc/httpd/conf/httpd-instance1.conf
+    sed -i 's/Listen 80/Listen 8082/' /etc/httpd/conf/httpd-instance2.conf
+
+    # Che
+    ln -s "/etc/httpd/sites-available/site1.otus.conf" "/etc/httpd/sites-enabled/site1.otus.conf"
+    ln -s "/etc/httpd/sites-available/site2.otus.conf" "/etc/httpd/sites-enabled/site2.otus.conf"
+
+    rm "/etc/httpd/conf.d/welcome.conf"
+
+    sed -i "s/SELINUX=.*/SELINUX=permissive/" /etc/selinux/config
+    reboot
 
 
 #sed -i '/ServerRoot "\/etc\/httpd"/a PidFile \/var\/run\/httpd-second.pid' /etc/httpd/conf/httpd-second.conf
@@ -51,37 +67,8 @@ sed -i 's/Listen 80/Listen 8082/' /etc/httpd/conf/httpd2.conf
 }
 
 
-function task3v2
-{
-    #yum install -y policycoreutils policycoreutils-python
-
-    echo "127.0.0.1 site1.otus site2.otus site3.otus" >> "/etc/hosts"
-
-
-    # Настройка главного файла конфигурации
-    echo 'IncludeOptional sites-enabled/*.conf' >> "/etc/httpd/conf/httpd.conf"
-    rm "/etc/httpd/conf.d/welcome.conf"
-
-    ln -s "/etc/httpd/sites-available/site1.otus.conf" \
-        "/etc/httpd/sites-enabled/site1.otus.conf"
-    ln -s "/etc/httpd/sites-available/site2.otus.conf" \
-        "/etc/httpd/sites-enabled/site2.otus.conf"
-    ln -s "/etc/httpd/sites-available/site3.otus.conf" \
-        "/etc/httpd/sites-enabled/site3.otus.conf"
-
-    #setsebool -P httpd_unified 1
-    #semanage port -a -t http_port_t -p tcp 8081
-    #semanage port -a -t http_port_t -p tcp 8082
-    #semanage port -a -t http_port_t -p tcp 8083
-
-    sed -i "s/SELINUX=.*/SELINUX=permissive/" /etc/selinux/config
-    reboot
-    #systemctl restart httpd && systemctl enable httpd
-
-}
 yum install -y mc nano tree
 
 task1
 task2
 task3v1
-task3v2
