dcb3314f046e3714753d44c6120f1c848020cd92
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-11-20 23:47:54 +0300

    Fix

--- lesson_09/provision.sh
+++ lesson_09/provision.sh
@@ -9,7 +9,7 @@ KEY_PUB=$KEY'.pub'
 case $HOSTNAME in
     $ANSIBLE_SERVER)
             yum install -y epel-release
-            yum install -y ansible sshpass mc nano tmux tree
+            yum install -y ansible sshpass mc nano tmux tree wget
             # Возможность использования имен серверов вместо IP-адресов
             echo "192.168.1.21  $SRV1" >> /etc/hosts
             echo "192.168.1.22  $SRV2" >> /etc/hosts
@@ -18,10 +18,13 @@ case $HOSTNAME in
             sed -i '35s/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' \
                 /etc/ssh/ssh_config
 
-            # Чтобы не вводить пароль пир добавлении публичного ключа
+            # Чтобы не вводить пароль при добавлении публичного ключа
             runuser -l vagrant -c "ssh-keygen -t rsa -N '' -b 2048 -f $KEY"
             runuser -l vagrant -c "sshpass -p vagrant ssh-copy-id -i $KEY_PUB $SRV1"
             runuser -l vagrant -c "sshpass -p vagrant ssh-copy-id -i $KEY_PUB $SRV2"
+
+            cp /vagrant/ansible-project /home/vagrant
+            chown -R vagrant:vagrant /home/vagrant/ansible-project
             ;;
 
     $SRV1)  sed -i '65s/PasswordAuthentication no/PasswordAuthentication yes/g' \
