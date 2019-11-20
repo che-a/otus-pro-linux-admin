fbbb9952019284f6e210d86d4fd977c9b73554a6
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-11-20 23:27:17 +0300

    Исправлены мелкие недочеты, повторяющиеся данные заменены на переменные.

--- lesson_09/provision.sh
+++ lesson_09/provision.sh
@@ -1,26 +1,36 @@
 #!/usr/bin/env bash
 
+SRV1='srv1'
+SRV2='srv2'
+ANSIBLE_SERVER='ansible'
+KEY='/home/vagrant/.ssh/id_rsa'
+KEY_PUB=$KEY'.pub'
 
 case $HOSTNAME in
-    ansible)    yum install -y sshpass mc nano #tmux tree
-                echo "192.168.1.21  srv1" >> /etc/hosts
-                echo "192.168.1.22  srv2" >> /etc/hosts
-                sed -i '35s/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' \
-                    /etc/ssh/ssh_config
-                systemctl restart sshd
+    $ANSIBLE_SERVER)
+            yum install -y epel-release
+            yum install -y ansible sshpass mc nano tmux tree
+            # Возможность использования имен серверов вместо IP-адресов
+            echo "192.168.1.21  $SRV1" >> /etc/hosts
+            echo "192.168.1.22  $SRV2" >> /etc/hosts
+            # Запретить SSH-клиенту при подключении к хосту осуществлять
+            # проверку подлинности его ключа.
+            sed -i '35s/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' \
+                /etc/ssh/ssh_config
 
-                runuser -l vagrant -c 'ssh-keygen -t rsa -N "" -b 2048 -f "/home/vagrant/.ssh/id_rsa"'
-                runuser -l vagrant -c 'sshpass -p "vagrant" ssh-copy-id -i "/home/vagrant/.ssh/id_rsa.pub" srv1'
-                runuser -l vagrant -c 'sshpass -p "vagrant" ssh-copy-id -i "/home/vagrant/.ssh/id_rsa.pub" srv2'
-                ;;
+            # Чтобы не вводить пароль пир добавлении публичного ключа
+            runuser -l vagrant -c "ssh-keygen -t rsa -N '' -b 2048 -f $KEY"
+            runuser -l vagrant -c "sshpass -p vagrant ssh-copy-id -i $KEY_PUB $SRV1"
+            runuser -l vagrant -c "sshpass -p vagrant ssh-copy-id -i $KEY_PUB $SRV2"
+            ;;
 
-    srv1)       sed -i '65s/PasswordAuthentication no/PasswordAuthentication yes/g' \
-                    /etc/ssh/sshd_config
-                systemctl restart sshd.service
-                ;;
+    $SRV1)  sed -i '65s/PasswordAuthentication no/PasswordAuthentication yes/g' \
+                /etc/ssh/sshd_config
+            systemctl restart sshd.service
+            ;;
 
-    srv2)       sed -i '123s/PasswordAuthentication no/PasswordAuthentication yes/g' \
-                    /etc/ssh/sshd_config
-                systemctl restart sshd.service
-                ;;
+    $SRV2)  sed -i '123s/PasswordAuthentication no/PasswordAuthentication yes/g' \
+                /etc/ssh/sshd_config
+            systemctl restart sshd.service
+            ;;
 esac
