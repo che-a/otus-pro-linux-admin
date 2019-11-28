9dd125ca5e5a2fcdca9e256378c4bd5707bc4d1a
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-11-28 05:38:14 +0300

    Correct Bacula-server, Bacula-client is not ready.

--- tasks/14/provision.sh
+++ tasks/14/provision.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-CLIENT='client'
-SERVER='server'
+CLIENT_NAME='cl'
+SERVER_NAME='srv'
+CLIENT_IP='192.168.50.20'
 KEY='/home/vagrant/.ssh/id_rsa'
 KEY_PUB=$KEY'.pub'
 
 case $HOSTNAME in
-    $SERVER)
+    $SERVER_NAME)
         yum install -y epel-release
         yum install -y ansible ansible-lint sshpass mc nano tmux tree wget
         # Возможность использования имен серверов вместо IP-адресов
-        echo "192.168.50.11  $CLIENT" >> /etc/hosts
+        echo "$CLIENT_IP  $CLIENT_NAME" >> /etc/hosts
         # Запретить SSH-клиенту при подключении к хосту осуществлять
         # проверку подлинности его ключа.
         sed -i '35s/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' \
@@ -18,13 +19,13 @@ case $HOSTNAME in
 
         # Чтобы не вводить пароль при добавлении публичного ключа
         runuser -l vagrant -c "ssh-keygen -t rsa -N '' -b 2048 -f $KEY"
-        runuser -l vagrant -c "sshpass -p vagrant ssh-copy-id -i $KEY_PUB $CLIENT"
+        runuser -l vagrant -c "sshpass -p vagrant ssh-copy-id -i $KEY_PUB $CLIENT_NAME"
 
         cp -r /vagrant/ansible-bacula/ /home/vagrant/
         chown -R vagrant:vagrant /home/vagrant/ansible-bacula
         ;;
 
-    $CLIENT)
+    $CLIENT_NAME)
         sed -i '65s/PasswordAuthentication no/PasswordAuthentication yes/g' \
             /etc/ssh/sshd_config
         systemctl restart sshd.service
