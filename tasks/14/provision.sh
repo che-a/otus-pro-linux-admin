be84f6b30d09146ac4341914e525695c7fccaa52
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-11-28 20:54:57 +0300

    Fix

--- tasks/14/provision.sh
+++ tasks/14/provision.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-CLIENT_NAME='cl'
+CLIENT_NAME='client1'
 SERVER_NAME='srv'
 CLIENT_IP='192.168.50.20'
-KEY='/home/vagrant/.ssh/id_rsa'
+KEY='.ssh/id_rsa'
 KEY_PUB=$KEY'.pub'
 
 case $HOSTNAME in
     $SERVER_NAME)
         yum install -y epel-release
-        yum install -y ansible ansible-lint sshpass nano tmux
+        yum install -y ansible ansible-lint sshpass
         # Возможность использования имен серверов вместо IP-адресов
         echo "$CLIENT_IP  $CLIENT_NAME" >> /etc/hosts
         # Запретить SSH-клиенту при подключении к хосту осуществлять
@@ -18,8 +18,12 @@ case $HOSTNAME in
             /etc/ssh/ssh_config
 
         # Чтобы не вводить пароль при добавлении публичного ключа
-        runuser -l vagrant -c "ssh-keygen -t rsa -N '' -b 2048 -f $KEY"
-        runuser -l vagrant -c "sshpass -p vagrant ssh-copy-id -i $KEY_PUB $CLIENT_NAME"
+        # - для пользователя vagrant
+        runuser -l vagrant -c "ssh-keygen -t rsa -N '' -b 2048 -f /home/vagrant/$KEY"
+        runuser -l vagrant -c "sshpass -p vagrant ssh-copy-id -i /home/vagrant/$KEY_PUB $CLIENT_NAME"
+        # - для пользователя root
+        ssh-keygen -t rsa -N '' -b 2048 -f /root/$KEY
+        sshpass -p vagrant ssh-copy-id -i /root/$KEY_PUB $CLIENT_NAME
 
         cp -r /vagrant/ansible-bacula/ /home/vagrant/
         chown -R vagrant:vagrant /home/vagrant/ansible-bacula
