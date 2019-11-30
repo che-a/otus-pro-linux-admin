cd736569f6126a3f248161e8ecc833f434fc9bc5
Aleksey Chernyshenko <tech.dep.it@ya.ru>
2019-11-30 06:48:59 +0300

    Fix

--- tasks/15/nginx-php7/phpinfo.php
+++ tasks/15/nginx-php7/phpinfo.php
@@ -1,22 +1,3 @@
-<!DOCTYPE html>
-<html lang="ru">
-    <head>
-        <meta charset="utf-8" />
-        <title>Docker</title>
-    </head>
-    <body>
-        <h2>OTUS. Linux Admin</h2>
-        <hr />
-        <div>
-            My custom PHP-page for lesson 15.
-        </div>
-        <div>
-            <?php
-                phpinfo();
-            ?>
-        </div>
-        <footer>
-            2019
-        </footer>
-    </body>
-</html>
+<?php
+    phpinfo();
+?>
