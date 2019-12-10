01509332070e3c2e07d9c5d9e7e4586e82465c63
Алексей Чернышенко <tech.dep.it@ya.ru>
2019-12-11 02:28:01 +0300

    Fix

--- tasks/19/provision.sh
+++ tasks/19/provision.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
 SRV1='client.linux.otus'
-ANSIBLE_SERVER='ipa-srv.linux.otus'
 SRV1_IP='192.168.50.20'
+ANSIBLE_SERVER='ipa-srv.linux.otus'
+ANSIBLE_SERVER_IP='192.168.50.10'
 KEY='/home/vagrant/.ssh/id_rsa'
 KEY_PUB=$KEY'.pub'
 
 case $HOSTNAME in
     $ANSIBLE_SERVER)
+        yum update -y
         yum install -y epel-release
         yum install -y ansible ansible-lint nano sshpass tmux
+        yum install -y ipa-server ipa-server-dns
         # Возможность использования имен серверов вместо IP-адресов
-        echo "$SRV1_IP  $SRV1" >> /etc/hosts
+        echo "$SRV1_IP  $SRV1 client" >> /etc/hosts
+        echo "$ANSIBLE_SERVER_IP  $ANSIBLE_SERVER ipa-srv" >> /etc/hosts
+
+        sed -i '1s/'$ANSIBLE_SERVER'/ /g' /etc/hosts
         # Запретить SSH-клиенту при подключении к хосту осуществлять
         # проверку подлинности его ключа.
         sed -i '35s/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' \
@@ -22,6 +28,16 @@ case $HOSTNAME in
         runuser -l vagrant -c "sshpass -p vagrant ssh-copy-id -i $KEY_PUB $SRV1"
         #cp -r /vagrant/ansible-log/ /home/vagrant/
         #chown -R vagrant:vagrant /home/vagrant/ansible-log
+
+        # Запускаем установку FreeIPA Server в режиме автоответа, передав необходимые параметры:
+        # -a password    #пароль учетной записи admin
+        # --hostname=ipaserver.test.lab   #FQDN имя сервера
+        # -r TEST.LAB  #Kerberos realm name (доменное имя большими буквами)
+        # -p password  #The kerberos master password
+        # -n test.lab  #доменное имя
+        # -U  #установка в режиме автоответа
+        #ipa-server-install -a password --hostname=ipaserver.test.lab -r TEST.LAB -p password -n test.lab -U
+#        ipa-server-install -a password --hostname=$ANSIBLE_SERVER -r LINUX.OTUS -p password -n linux.otus -U
         ;;
 
     $SRV1)
@@ -33,4 +49,4 @@ esac
 
 yes | cp -rf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
-reboot
+#reboot
