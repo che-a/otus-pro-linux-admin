dd759b3685b991d459b0a2930bb393d7e25c8ae8
Алексей Чернышенко <tech.dep.it@ya.ru>
2019-12-10 12:24:32 +0300

    Add

---
+++ tasks/19/provision.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+SRV1='client.linux.otus'
+ANSIBLE_SERVER='ipa-srv.linux.otus'
+SRV1_IP='192.168.50.20'
+KEY='/home/vagrant/.ssh/id_rsa'
+KEY_PUB=$KEY'.pub'
+
+case $HOSTNAME in
+    $ANSIBLE_SERVER)
+        yum install -y epel-release
+        yum install -y ansible ansible-lint nano sshpass tmux
+        # Возможность использования имен серверов вместо IP-адресов
+        echo "$SRV1_IP  $SRV1" >> /etc/hosts
+        # Запретить SSH-клиенту при подключении к хосту осуществлять
+        # проверку подлинности его ключа.
+        sed -i '35s/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' \
+            /etc/ssh/ssh_config
+
+        # Чтобы не вводить пароль при добавлении публичного ключа
+        runuser -l vagrant -c "ssh-keygen -t rsa -N '' -b 2048 -f $KEY"
+        runuser -l vagrant -c "sshpass -p vagrant ssh-copy-id -i $KEY_PUB $SRV1"
+        #cp -r /vagrant/ansible-log/ /home/vagrant/
+        #chown -R vagrant:vagrant /home/vagrant/ansible-log
+        ;;
+
+    $SRV1)
+        sed -i '65s/PasswordAuthentication no/PasswordAuthentication yes/g' \
+            /etc/ssh/sshd_config
+        systemctl restart sshd.service
+        ;;
+esac
+
+yes | cp -rf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+reboot
