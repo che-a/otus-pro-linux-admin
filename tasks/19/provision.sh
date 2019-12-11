d9e14da043d0fdf3490e8427603f1e536639670e
Алексей Чернышенко <tech.dep.it@ya.ru>
2019-12-11 11:11:10 +0300

    Fix

--- tasks/19/provision.sh
+++ tasks/19/provision.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 
-SRV1='client.linux.otus'
-SRV1_IP='192.168.50.20'
-ANSIBLE_SERVER='ipa-srv.linux.otus'
-ANSIBLE_SERVER_IP='192.168.50.10'
+DOMAIN='linux.otus'
+REALM_NAME='LINUX.OTUS'
+NET='192.168.50'
+
+CLIENT='client'
+CLIENT_FULL=$CLIENT'.'$DOMAIN
+CLIENT_IP=$NET'.20'
+SERVER='ipa'
+SERVER_FULL=$SERVER'.'$DOMAIN
+SERVER_IP=$NET'.10'
+
 KEY='/home/vagrant/.ssh/id_rsa'
 KEY_PUB=$KEY'.pub'
 
+
 case $HOSTNAME in
-    $ANSIBLE_SERVER)
+    'server')
         yum update -y
         yum install -y epel-release
         yum install -y ansible ansible-lint nano sshpass tmux
         yum install -y ipa-server ipa-server-dns
-        # Возможность использования имен серверов вместо IP-адресов
-        echo "$SRV1_IP  $SRV1 client" >> /etc/hosts
-        echo "$ANSIBLE_SERVER_IP  $ANSIBLE_SERVER ipa-srv" >> /etc/hosts
 
-        sed -i '1s/'$ANSIBLE_SERVER'/ /g' /etc/hosts
+        # Возможность использования имен серверов вместо IP-адресов
+        echo "$SERVER_IP  $SERVER_FULL $SERVER" > /etc/hosts
+        echo "$CLIENT_IP  $CLIENT_FULL $CLIENT" >> /etc/hosts
+
         # Запретить SSH-клиенту при подключении к хосту осуществлять
         # проверку подлинности его ключа.
         sed -i '35s/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' \
@@ -25,22 +33,24 @@ case $HOSTNAME in
 
         # Чтобы не вводить пароль при добавлении публичного ключа
         runuser -l vagrant -c "ssh-keygen -t rsa -N '' -b 2048 -f $KEY"
-        runuser -l vagrant -c "sshpass -p vagrant ssh-copy-id -i $KEY_PUB $SRV1"
-        #cp -r /vagrant/ansible-log/ /home/vagrant/
-        #chown -R vagrant:vagrant /home/vagrant/ansible-log
+        runuser -l vagrant -c "sshpass -p vagrant ssh-copy-id -i $KEY_PUB $CLIENT"
 
-        # Запускаем установку FreeIPA Server в режиме автоответа, передав необходимые параметры:
-        # -a password    #пароль учетной записи admin
-        # --hostname=ipaserver.test.lab   #FQDN имя сервера
-        # -r TEST.LAB  #Kerberos realm name (доменное имя большими буквами)
-        # -p password  #The kerberos master password
-        # -n test.lab  #доменное имя
-        # -U  #установка в режиме автоответа
-        #ipa-server-install -a password --hostname=ipaserver.test.lab -r TEST.LAB -p password -n test.lab -U
-#        ipa-server-install -a password --hostname=$ANSIBLE_SERVER -r LINUX.OTUS -p password -n linux.otus -U
+        # cp -r /vagrant/ansible-log/ /home/vagrant/
+        # chown -R vagrant:vagrant /home/vagrant/ansible-log
+
+#        ipa-server-install  --hostname=$SERVER_FULL \
+#                            --domain=$DOMAIN \
+#                            --realm=$REALM_NAME \
+#                            --ds-password=password1234 \
+#                            --admin-password=password1234 \
+#                            --mkhomedir \
+#                            --setup-dns \
+#                            --forwarder=77.88.8.8 \
+#                            --auto-reverse \
+#                            --unattended
         ;;
 
-    $SRV1)
+    'client')
         sed -i '65s/PasswordAuthentication no/PasswordAuthentication yes/g' \
             /etc/ssh/sshd_config
         systemctl restart sshd.service
@@ -48,5 +58,6 @@ case $HOSTNAME in
 esac
 
 yes | cp -rf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
-#reboot
+reboot
