e6557f27696a02d5d8f386bda90bc6dee7860edd
Алексей Чернышенко <tech.dep.it@ya.ru>
2019-12-16 13:14:48 +0300

    fix

--- tasks/19/files_all_vms/opt/provision.sh
+++ tasks/19/files_all_vms/opt/provision.sh
@@ -5,128 +5,116 @@
 # - на Ansible-сервере устанавливается сервер имен;
 # - с Ansible-сервера есть беспарольный доступ по SSH-ключам на каждую машину.
 
-DOMAIN='linux.otus'
-HOST_02='gw'    ; HOST_02_FULL=$HOST_02'.'$DOMAIN   # Ansible-клиент
-HOST_01='ipa'   ; HOST_01_FULL=$HOST_01'.'$DOMAIN   # Ansible-клиент
-HOST_00='ns1'   ; HOST_00_FULL=$HOST_00'.'$DOMAIN   # Ansible-сервер
-
+DOMAIN="linux.otus"
+NS1='ns1'       ; NS1_FULL=$NS1'.'$DOMAIN           # Клиент Ansible
+HOST2='gw'      ; HOST2_FULL=$HOST2'.'$DOMAIN       # Клиент Ansible
+HOST1='ipa'     ; HOST1_FULL=$HOST1'.'$DOMAIN       # Клиент Ansible
+ANSIBLE='mgmt'  ; ANSIBLE_FULL=$ANSIBLE'.'$DOMAIN   # Ansible-сервер
 # Машины, на которые будут добавлены публичные SSH-ключи
-HOSTS_FULL=( $HOST_02_FULL $HOST_01_FULL )
+HOSTS_FULL=( $NS1_FULL $HOST2_FULL $HOST1_FULL )
+#HOSTS_FULL=( $NS1_FULL )
 
-NET='192.168.50'    ; HOST_02_IP=$NET'.1'
-                      HOST_01_IP=$NET'.10'
-                      HOST_00_IP=$NET'.50'
+NS1_IP="192.168.50.50"
 
 KEY='/home/vagrant/.ssh/id_rsa'    ; KEY_PUB=$KEY'.pub'
 ENV_FILE='/etc/sysconfig/provision.env'
 
-NAME=$1     # В процессе настроки имя меняется, поэтому используется переменная в env-файле
-STAGE=$2
-
-
-function config_ssh_access
-{
-    #statements
-    return
-}
-
-
 # Из-за необходимости промежуточной перезагрузки процесс настройки каждой ВМ
 # делится на этапы ($STAGE), чтобы после перезагрузки продолжать настройку не
 # с начала.
+STAGE=$2
 function stage_up
 {
     sed -i 's/STAGE=.*/STAGE="'$(( $STAGE + 1 ))'"/' $ENV_FILE
 }
 
 
+# В процессе настроки hostname меняется и, чтобы не было неоднозначности,
+# используется переменная с именем машины в env-файле
+NAME=$1
 case $NAME in
-    $HOST_02)
+    $NS1)
+        case $STAGE in
+            0)  cp -rf /home/vagrant/bind/* /
+                yum install -y epel-release
+                yum install -y bind bind-utils nano
+                yum update -y
+
+                # Чтобы не вводить пароль на сервере при добавлении публичного
+                # ключа с него на этот хост
+                sed -i '65s/PasswordAuthentication no/PasswordAuthentication yes/g' \
+                    /etc/ssh/sshd_config
+
+                echo $NAME'.'$DOMAIN > /etc/hostname
+                echo 'PEERDNS="no"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+                echo "DNS1=$NS1_IP" >> /etc/sysconfig/network-scripts/ifcfg-eth1
+
+                systemctl disable provision.service
+                systemctl enable named.service
+                stage_up
+                reboot
+                ;;
+            *)  exit
+                ;;
+        esac
+        ;;
+    $HOST2|$HOST1)
         case $STAGE in
             0)  yum install -y epel-release
                 yum install -y bind-utils nano
-                #yum update -y
-
-                sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
-                #yes | cp -rf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-                # Чтобы не вводить пароль на сервере при добавлении публичного
-                # ключа с него на этот хост
-                sed -i '65s/PasswordAuthentication no/PasswordAuthentication yes/g' \
-                    /etc/ssh/sshd_config
-
-                echo $NAME'.'$DOMAIN > /etc/hostname
-                echo 'PEERDNS="no"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
-                echo "DNS1=$HOST_00_IP" >> /etc/sysconfig/network-scripts/ifcfg-eth1
-
-                stage_up
-                systemctl disable provision.service
-                reboot
-                ;;
-            *)  exit
-                ;;
-        esac
-        ;;
-
-    $HOST_01)
-        case $STAGE in
-            0)  yum update -y
-                yum install -y epel-release
-                yum install -y bind-utils ipa-server nano
-
-                sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
-                #yes | cp -rf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-                # Чтобы не вводить пароль на сервере при добавлении публичного
-                # ключа с него на этот хост
-                sed -i '65s/PasswordAuthentication no/PasswordAuthentication yes/g' \
-                    /etc/ssh/sshd_config
-
-                echo $NAME'.'$DOMAIN > /etc/hostname
-                echo 'PEERDNS="no"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
-                echo "DNS1=$HOST_00_IP" >> /etc/sysconfig/network-scripts/ifcfg-eth1
-                sed -i 's/DEFROUTE=.*/DEFROUTE="no"/' /etc/sysconfig/network-scripts/ifcfg-eth1
-
-                stage_up
-                systemctl disable provision.service
-                reboot
-                ;;
-            *)  exit
-                ;;
-        esac
-        ;;
-    $HOST_00)
-        case $STAGE in
-            0)  # cp -r /vagrant/ansible-freeipa/ /home/vagrant/
-                # chown -R vagrant:vagrant /home/vagrant/ansible-freeipa
-
-                yum install -y epel-release
-                yum install -y ansible ansible-lint bind bind-utils nano sshpass
                 yum update -y
-                yes | cp -rf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+
+                sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+                # Чтобы не вводить пароль на сервере при добавлении публичного
+                # ключа с него на этот хост
+                sed -i '65s/PasswordAuthentication no/PasswordAuthentication yes/g' \
+                    /etc/ssh/sshd_config
+
+                echo $NAME'.'$DOMAIN > /etc/hostname
+                echo 'PEERDNS="no"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+                echo "DNS1=$NS1_IP" >> /etc/sysconfig/network-scripts/ifcfg-eth1
+
+                systemctl disable provision.service
+                stage_up
+                reboot
+                ;;
+            *)  exit
+                ;;
+        esac
+        ;;
+    $ANSIBLE)
+        case $STAGE in
+            0)  chown -R vagrant:vagrant /home/vagrant/ansible
+                yum install -y epel-release
+                yum install -y ansible ansible-lint bind-utils nano sshpass
+                yum update -y
                 # Запретить SSH-клиенту этой машины при подключении к хосту
                 # осуществлять проверку подлинности его ключа.
                 sed -i '35s/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' \
                     /etc/ssh/ssh_config
-                systemctl enable named
-                echo $HOST_00_FULL > /etc/hostname
+
+                echo $NAME'.'$DOMAIN > /etc/hostname
                 echo 'PEERDNS="no"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
-                echo "DNS1=$HOST_00_IP" >> /etc/sysconfig/network-scripts/ifcfg-eth1
+                echo "DNS1=$NS1_IP" >> /etc/sysconfig/network-scripts/ifcfg-eth1
+
                 stage_up
                 reboot
                 ;;
             1)  runuser -l vagrant -c "ssh-keygen -t rsa -N '' -b 2048 -f $KEY"
                 for HOST_FULL in ${HOSTS_FULL[@]}; do
-                    # Машина, на которую необходимо скопировать публичный ключ
-                    # может быть недоступна, поэтому необходимо пробовать пока
-                    # не получится.
-                    while [ 1 ]; do
+                    # Машина, на которую необходимо скопировать публичный ключ,
+                    # может быть недоступна в даный момент, поэтому производится
+                    # несколько попыток.
+                    for attempt in $(seq 1 100); do
                         runuser -l vagrant -c "sshpass -p vagrant ssh-copy-id -i $KEY_PUB $HOST_FULL"
                         if [ "$?" -eq 0 ]; then
                             break
                         fi
+                        sleep 1
                     done
                 done
-                stage_up
                 systemctl disable provision.service
+                stage_up
                 ;;
             *)  exit
                 ;;
